ec3471ff1c0ff36d0b5e2a518edd0ce2ca6ddc9f
ramong26 <rlatndus21049@gmail.com>
2026-05-13 00:01:12 +0900

    fix(prettier): 프리티어 재설정 - 기존 `prettier` 파일에서 import 순서와 테일윈드 순서가 충돌이 일어나 해당 사항 수정
    - prettier 에서는 테일윈드 순서 정렬 담당
    - eslint 에서는 오류를 보여주고 setting.json에서 해당 오류를 고치도록 로직 변경
    - 해당 문제는 자주 일어나는 충돌 문제로 보임

--- package/ui/eslint.config.mjs
+++ package/ui/eslint.config.mjs
@@ -1,4 +1,22 @@
-import { config } from "@repo/eslint-config/react-internal";
+import tseslint from 'typescript-eslint';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
 
-/** @type {import("eslint").Linter.Config} */
-export default config;
+export default tseslint.config(
+  ...tseslint.configs.recommended,
+
+  {
+    files: ['**/*.{ts,tsx}'],
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [['^react$'], ['^@radix-ui'], ['^@repo'], ['^\\w'], ['^\\.']],
+        },
+      ],
+      'simple-import-sort/exports': 'error',
+    },
+  },
+);
