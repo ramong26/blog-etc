a8d36229562d9938f7338c6769094ee142d78288
ramong26 <rlatndus21049@gmail.com>
2026-05-20 22:26:32 +0900

    refactor(header): 메뉴 렌더링 구조 정리

--- frontend/widgets/header/model/header-menu.ts
+++ frontend/widgets/header/model/header-menu.ts
@@ -1,5 +1,10 @@
 import { ROUTES } from '@/shared/constants/route';
 
+/**
+ * @description Header menu
+ * label: 메뉴에 표시될 텍스트
+ * href: 메뉴 클릭 시 이동할 경로
+ */
 export const HEADER_MENU = [
   {
     label: 'HOME',
@@ -22,3 +27,6 @@ export const HEADER_MENU = [
     href: ROUTES.RESUME,
   },
 ];
+
+type HeaderMenuType = typeof HEADER_MENU;
+export type HeaderMenuItemType = HeaderMenuType[number];
--- frontend/widgets/header/ui/Header.tsx
+++ frontend/widgets/header/ui/Header.tsx
@@ -1,13 +1,24 @@
-import Link from 'next/link';
+'use client';
 
-import { HEADER_MENU } from '../model/header-menu';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+import { cn } from '@repo/ui';
+
+import { HEADER_MENU, HeaderMenuItemType } from '../model/header-menu';
 
 export function Header() {
+  const pathName = usePathname();
+
   return (
-    <header className="mx-auto w-full border-b-2 border-border-strong py-4">
-      <nav className="flex items-center justify-center gap-20">
-        {HEADER_MENU.map((item) => (
-          <HeaderMenu key={item.href} label={item.label} href={item.href} />
+    <header className="mx-auto w-full border-b-2 border-border-strong py-2">
+      <nav className="flex items-center justify-center gap-10">
+        {HEADER_MENU.map((menuItem) => (
+          <HeaderMenu
+            key={menuItem.href}
+            item={menuItem}
+            isActive={pathName === menuItem.href}
+          />
         ))}
       </nav>
     </header>
@@ -15,16 +26,24 @@ export function Header() {
 }
 
 interface HeaderMenuProps {
-  label: string;
-  href: string;
+  item: HeaderMenuItemType;
+  isActive: boolean;
 }
-function HeaderMenu({ label, href }: HeaderMenuProps) {
+function HeaderMenu({ item, isActive }: HeaderMenuProps) {
   return (
     <Link
-      href={href}
-      className="text-2xl font-medium text-primary transition-all duration-200 hover:font-bold hover:text-accent-red"
+      href={item.href}
+      className="group p-4 text-2xl font-medium transition-all duration-200 hover:bg-accent-red hover:text-text-inverse"
     >
-      {label}
+      <span
+        className={cn(
+          'border-b-2 border-transparent pb-3 transition-all duration-200',
+          isActive &&
+            'border-accent-red text-accent-red group-hover:text-text-inverse',
+        )}
+      >
+        {item.label}
+      </span>
     </Link>
   );
 }
